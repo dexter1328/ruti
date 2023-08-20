6183f3bb9b51da8208155bdacf56038d1ed2b89c
muhammadaatir <aatirmemon.8211@gmail.com>
2023-08-21 02:34:35 +0500

    front end changes in marketplace

--- resources/views/vendor/marketplace/index.blade.php
+++ resources/views/vendor/marketplace/index.blade.php
@@ -10,12 +10,71 @@
                 <form class="d-flex align-items-center" action="{{route('vendor.product-search')}}" method="get">
                     <input class="form-control search_bar" placeholder="Search Products" aria-label="Search" type="text" name="query" id="query" value="{{request()->input('query')}}" >
                     <button class="btn button_color search_btn" type="submit"><i class="fa fa-search"></i></button>
+                    <div class="input-group-append">
+                        <button class="btn button_color search_btn dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-filter"></i></button>
+                        <div class="dropdown-menu p-3">
+                            <div class='d-flex'>
+                                <form action="{{route('vendor.product-search')}}" method="get">
+                                <div class='filters_sec mr-3'>
+                                    <h4>Product Categories:</h4>
+                                    @foreach ($categories as $c)
+                                    <ul class="mb-0">
+                                    <input type="checkbox" name="category_name[]" value="{{ $c->category1 }}"> {{ $c->category1 }}
+                                    </ul>
+                                    @endforeach
+                                </div>
+                                <div class='filters_sec mr-3'>
+                                    <h4>Status:</h4>
+                                    <span>
+                                        <input type="radio" name="first_section" value="all">
+                                        <label for="">All</label>
+                                    </span>
+                                    <span>
+                                        <input type="radio" name="first_section" value="active">
+                                        <label for="">Active</label>
+                                        </span>
+                                    <span>
+                                        <input type="radio" name="first_section" value="inactive">
+                                        <label for="">InActive</label>
+                                        </span>
+                                    <span>
+                                        <input type="radio" name="first_section" value="incomplete">
+                                        <label for="">Incomplete</label>
+                                        </span>
+                                    <span>
+                                        <input type="radio" name="first_section" value="listing_removed">
+                                        <label for="">Listing Removed</label>
+                                        </span>
+                                    <span>
+                                        <input type="radio" name="first_section" value="listing_removed">
+                                        <label for="">Search Suppressed</label>
+                                    </span>
+                                </div>
+                                <div class='filters_sec mr-3'>
+                                    <h4>Fulfilled By:</h4>
+                                    <span>
+                                        <input type="radio" name="second_section" value="all">
+                                        <label for="">All</label>
+                                    </span>
+                                    <span>
+                                        <input type="radio" name="second_section" value="amazon">
+                                        <label for="">Nature Checkout</label>
+                                    </span>
+                                    <span>
+                                        <input type="radio" name="second_section" value="merchant">
+                                        <label for="">Vendor</label>
+                                    </span>
+                                </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
                   <span class="ml-2"><b>{{$supplier_products->count()}}</b> product(s)</span>
                 </form>
             </div>
         </div>
         <div class="row mx-0">
-        <div class="col-lg-6">
+        <div class="col-lg-12">
         <table class="m-2 mt-4 border mx-auto main_area_table">
             <tr class="table_head border-bottom">
                 <th colspan="3" class="p-3">Main Areas:
@@ -34,89 +93,13 @@
             </tr>
         </table>
         </div>
-        <div class="col-lg-6">
-        <table class="m-2 mt-4 border mx-auto main_area_table">
-            <tr class="table_head border-bottom">
-                <th colspan="3" class="p-3">Product Categories </th>
-            </tr>
-            <tr class="first_table_body">
-                <td class="px-4 py-4">
-                    <form action="{{route('vendor.product-search')}}" method="get">
-                    @foreach ($categories as $c)
-                        <ul class="mb-0">
-                            <input type="checkbox" name="category_name[]" value="{{ $c->category1 }}"> {{ $c->category1 }}
-
-                        </ul>
-
-                    @endforeach
-                    <button type="submit">submit</button>
-                </form>
-                </td>
-            </tr>
-        </table>
         </div>
-        </div>
-        <div class="d-flex filter_div mx-2 my-4 justify-content-between p-3">
-            <span class="d-flex align-items-center w-100">
-            <form class="w-100 second_filter_form justify-content-around" action="">
-              <label>Status:</label>
-              <span>
-                  <input type="radio" name="first_section" value="all">
-                  <label for="">All</label>
-                </span>
-              <span>
-                  <input type="radio" name="first_section" value="active">
-                  <label for="">Active</label>
-                </span>
-              <span>
-                  <input type="radio" name="first_section" value="inactive">
-                  <label for="">InActive</label>
-                </span>
-              <span>
-                  <input type="radio" name="first_section" value="incomplete">
-                  <label for="">Incomplete</label>
-                </span>
-              <span>
-                  <input type="radio" name="first_section" value="listing_removed">
-                  <label for="">Listing Removed</label>
-                </span>
-              <span>
-                  <input type="radio" name="first_section" value="listing_removed">
-                  <label for="">Search Suppressed</label>
-                </span>
-            </form>
-        </div>
-        <div class="filter_div mx-2 my-4 justify-content-between p-3">
-            <form class="fulfil_filter d-flex justify-content-around" action="">
-                <label>Fulfilled By:</label>
-                <span>
-                    <input type="radio" name="second_section" value="all">
-                    <label for="">All</label>
-                </span>
-                <span>
-                    <input type="radio" name="second_section" value="amazon">
-                    <label for="">Nature Checkout</label>
-                </span>
-                <span>
-                    <input type="radio" name="second_section" value="merchant">
-                    <label for="">Vendor</label>
-                </span>
-            </form>
-            </span>
-            <span>
-                <label for="">Filter:</label>
-                <select name="" id="">
-                    <option value="">Additional Items</option>
-                    <option value="">Additional Items1</option>
-                    <option value="">Additional Items2</option>
-                </select>
-            </span>
-        </div>
-        <div class="row mx-0 table_overflow">
+        <div class="row mt-4 mx-0 table_overflow">
             <table class="details_table rounded col-12 border">
                 <tr class="table_head">
                     <th><input type="checkbox" name="" id=""></th>
                     <th>Status</th>
+                    <th>Quantity</th>
                     <th>Image</th>
                     <th>SKU</th>
                     <th>Product Name</th>
@@ -135,15 +118,16 @@
                 <tr>
                     <td><input type="checkbox" name="" id=""></td>
                     <td>{{$p->status == 'enable' ? 'active' : 'inactive'}}</td>
+                    <td><input type="number" value="1" disabled></td>
                     <td><img src="{{$p->original_image_url}}" class="product_image" alt=""></td>
                     <td>{{$p->sku}}</td>
                     <td>{{$p->title}}</td>
                     <td>{{$p->created_at->format('d/m/Y')}}<br> </td>
                     <td><input type="number" value="{{$p->stock}}" disabled></td>
                     <td>${{$pct}}</td>
-                    <td><input type="number" value="{{$p->wholesale_price}}" disabled>+$0.00</td>
-                    <td><input type="number"></td>
-                    <td>$50</td>
+                    <td><input type="number" id="wholesale_price" value="{{$p->wholesale_price}}" disabled>+$0.00</td>
+                    <td><input type="number" onblur={calculateRetailPrice()} id="profit_expected"></td>
+                    <td><input type="number" id="retail_price" disabled></td>
                     <td><button class="btn button_color">Edit</button></td>
                 </tr>
                 @endforeach
@@ -235,5 +219,15 @@
             </table>
         </div>
     </div>
-
+<script type="text/javascript">
+    var wholesale_price = document.getElementById('wholesale_price')
+    var profit_expected = document.getElementById('profit_expected')
+    var retail_price = document.getElementById('retail_price')
+    function calculateRetailPrice() {
+        console.log(retail_price.value)
+        if(profit_expected.value > 1) {
+            retail_price.value = profit_expected + wholesale_price
+        }
+    }
+</script>
 @endsection
