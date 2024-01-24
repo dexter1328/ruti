d4320ac3aa3170786b713047f3f984d4021ff0c8
Hassan Ahmed Khan <ihassanahmedkhan@gmail.com>
2024-01-24 20:39:13 +0500

    added faqs

--- resources/views/front_end/shop.blade.php
+++ resources/views/front_end/shop.blade.php
@@ -5,6 +5,97 @@
 {{-- @include('front_end.banner')
 @include('front_end.features') --}}
 
+<!-- THIS SECTION WILL BE MOVED TO A NEW PAGE
+ <div class="faq-section" id="faq-section">
+  <h2>Frequently Asked Questions</h2>
+            <div class="faq-item">
+                <h2>How do I place an order?</h2>
+                <p>
+                    Ordering from Nature Checkout is easy! Simply browse our
+                    products, click on the item you want, choose your
+                    options/specs (size, color, etc.), and click "Add to Cart."
+                    When you're ready to complete your purchase, click the
+                    shopping cart icon and follow the checkout process.
+                </p>
+            </div>
+
+            <div class="faq-item">
+                <h2>What payment methods do you accept?</h2>
+                <p>
+                    We accept major credit and debit cards, including Visa,
+                    Mastercard, American Express, and Discover. Additionally, we
+                    also accept PayPal for a convenient and secure checkout.
+                </p>
+            </div>
+
+            <div class="faq-item">
+                <h2>How long will the delivery take?</h2>
+                <p>
+                    The delivery time depends on your location and the shipping
+                    method you choose during the checkout process. Our standard
+                    shipping option typically takes between 2 to 10 business
+                    days, while express shipping delivers within 5 business
+                    days.
+                </p>
+            </div>
+
+            <div class="faq-item">
+                <h2>What is your return policy?</h2>
+                <p>
+                    If you're not completely satisfied with your purchase, we
+                    offer a 30-day return policy. Please review our
+                    <a href="#">Return Policy</a> for detailed instructions on
+                    returning items and processing refunds.
+                </p>
+            </div>
+
+            <div class="faq-item">
+                <h2>Do you ship internationally?</h2>
+                <p>
+                    Yes, we ship our products worldwide. Please note that
+                    shipping times and fees may vary depending on your location.
+                    For more information, visit our
+                    <a href="#">Shipping Policy</a>.
+                </p>
+            </div>
+
+            <div class="faq-item">
+                <h2>How can I track my order?</h2>
+                <p>
+                    To track your order, log in to your Nature Checkout account
+                    and navigate to the "Order History" section. There, you can
+                    find the tracking information for your purchase.
+                </p>
+            </div>
+
+            <div class="faq-item">
+                <h2>
+                    What if I have a question about a product or need
+                    assistance?
+                </h2>
+                <p>
+                    We're here to help! Feel free to contact our Customer
+                    Support team via email at info@naturecheckout.com or contact
+                    helpdesk during our business hours. We're always happy to
+                    assist with any inquiries or concerns.
+                </p>
+            </div>
+
+            <div class="faq-item">
+                <h2>
+                    How can I stay updated with Nature Checkout's latest
+                    products and promotions?
+                </h2>
+                <p>
+                    You can follow us on our social media platforms – Facebook,
+                    Instagram, Twitter, and Pinterest – or subscribe to our
+                    newsletter. By doing so, you'll be the first to know about
+                    new arrivals, promotions, and eco-conscious tips.
+                </p>
+            </div>
+        </div>
+
+ -->
 
 
 <div class="landing-slider">
@@ -949,99 +1040,6 @@
 </div>
 
 <!-- Blogs End -->
-
-<!-- THIS SECTION WILL BE MOVED TO A NEW PAGE
- <div class="faq-section" id="faq-section">
-  <h2>Frequently Asked Questions</h2>
-            <div class="faq-item">
-                <h2>How do I place an order?</h2>
-                <p>
-                    Ordering from Nature Checkout is easy! Simply browse our
-                    products, click on the item you want, choose your
-                    options/specs (size, color, etc.), and click "Add to Cart."
-                    When you're ready to complete your purchase, click the
-                    shopping cart icon and follow the checkout process.
-                </p>
-            </div>
-
-            <div class="faq-item">
-                <h2>What payment methods do you accept?</h2>
-                <p>
-                    We accept major credit and debit cards, including Visa,
-                    Mastercard, American Express, and Discover. Additionally, we
-                    also accept PayPal for a convenient and secure checkout.
-                </p>
-            </div>
-
-            <div class="faq-item">
-                <h2>How long will the delivery take?</h2>
-                <p>
-                    The delivery time depends on your location and the shipping
-                    method you choose during the checkout process. Our standard
-                    shipping option typically takes between 2 to 10 business
-                    days, while express shipping delivers within 5 business
-                    days.
-                </p>
-            </div>
-
-            <div class="faq-item">
-                <h2>What is your return policy?</h2>
-                <p>
-                    If you're not completely satisfied with your purchase, we
-                    offer a 30-day return policy. Please review our
-                    <a href="#">Return Policy</a> for detailed instructions on
-                    returning items and processing refunds.
-                </p>
-            </div>
-
-            <div class="faq-item">
-                <h2>Do you ship internationally?</h2>
-                <p>
-                    Yes, we ship our products worldwide. Please note that
-                    shipping times and fees may vary depending on your location.
-                    For more information, visit our
-                    <a href="#">Shipping Policy</a>.
-                </p>
-            </div>
-
-            <div class="faq-item">
-                <h2>How can I track my order?</h2>
-                <p>
-                    To track your order, log in to your Nature Checkout account
-                    and navigate to the "Order History" section. There, you can
-                    find the tracking information for your purchase.
-                </p>
-            </div>
-
-            <div class="faq-item">
-                <h2>
-                    What if I have a question about a product or need
-                    assistance?
-                </h2>
-                <p>
-                    We're here to help! Feel free to contact our Customer
-                    Support team via email at info@naturecheckout.com or contact
-                    helpdesk during our business hours. We're always happy to
-                    assist with any inquiries or concerns.
-                </p>
-            </div>
-
-            <div class="faq-item">
-                <h2>
-                    How can I stay updated with Nature Checkout's latest
-                    products and promotions?
-                </h2>
-                <p>
-                    You can follow us on our social media platforms – Facebook,
-                    Instagram, Twitter, and Pinterest – or subscribe to our
-                    newsletter. By doing so, you'll be the first to know about
-                    new arrivals, promotions, and eco-conscious tips.
-                </p>
-            </div>
-        </div>
-
-         /FAQs -->
-
 
     <script>
     // Product image slider script
